7aa1cf690c58dd13aafca897bb3eb7fc04fa17ac
ikunleo <leot20090109@icloud.com>
2025-10-28 15:22:34 +0800

    Home page: add informative landing index.html and wire root route; update copy

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(express.static('public'));
 
 // 👇加这一段
 app.get('/', (req, res) => {
-  res.redirect('/register.html');
+  res.sendFile(path.join(process.cwd(), 'public', 'index.html'));
 });
 
 // 便捷路由重定向
